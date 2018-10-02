967f400ec25ed5329fdd0adc1044da83a55c6872
meetshah1995 <meetshah1995@gmail.com>
2018-10-02 10:22:05 -0700

    Add bias as parameter for GCN

--- config/function_config_lib.py
+++ config/function_config_lib.py
@@ -46,8 +46,8 @@ gcn_finetune_faster_rcnn_fpn_fc7.bias_file = ""
 gcn_finetune_faster_rcnn_fpn_fc7.relations = []
 gcn_finetune_faster_rcnn_fpn_fc7.n_feats = ""
 gcn_finetune_faster_rcnn_fpn_fc7.branch_from = ""
-gcn_finetune_faster_rcnn_fpn_fc7.bias = ""
-gcn_finetune_faster_rcnn_fpn_fc7.temperature = ""
+gcn_finetune_faster_rcnn_fpn_fc7.bias = False
+gcn_finetune_faster_rcnn_fpn_fc7.temperature = 1.0
 
 
 default_feature = AttrDict()
